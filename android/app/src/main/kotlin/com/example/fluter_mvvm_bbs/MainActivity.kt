16055ce75b4e94da41172805943e14ea29ee8cde
Umigishi-Aoi <umigishi.aoi@gmail.com>
2023-09-19 07:46:48 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/fluter_mvvm_bbs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluter_mvvm_bbs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
