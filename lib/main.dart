ff63634768c015380bc7265d2f47cd0e5f0257d8
Umigishi-Aoi <umigishi.aoi@gmail.com>
2023-09-19 14:58:08 +0900

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
