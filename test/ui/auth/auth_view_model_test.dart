ed3b16da0af3a123a34612ae42cdcbe54c459627
Umigishi-Aoi <umigishi.aoi@gmail.com>
2023-09-20 06:58:02 +0900

    add auth test

---
+++ test/ui/auth/auth_view_model_test.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:fluter_mvvm_bbs/model/auth/auth_model.dart';
+import 'package:fluter_mvvm_bbs/ui/auth/auth_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final authModel = AuthModel(MockFirebaseAuth());
+
+  final authViewModel = AuthViewModel.withModel(authModel);
+
+  group('AuthViewModel Test', () {
+    test('sign in test', () {
+      expect(authViewModel.isLogin, false);
+
+      authViewModel.signInWithGitHub();
+
+      expect(authViewModel.isLogin, true);
+    });
+
+    test('sign out test', () {
+      authViewModel.signInWithGitHub();
+      expect(authViewModel.isLogin, true);
+
+      authViewModel.signOut();
+      expect(authViewModel.isLogin, false);
+    });
+  });
+}
